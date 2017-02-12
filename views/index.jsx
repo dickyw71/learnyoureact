5314a6895639536af74eff0ddb8d279badcc0a92
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-02-12 16:17:48 +0000

    Solution to CSS exercise.

--- views/index.jsx
+++ views/index.jsx
@@ -31,20 +31,20 @@ class TodoList extends React.Component {
 class Todo extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {checked: false};
+		this.state = { checked: false };
 	}
 	handleChange(e) {
-		this.setState({checked: e.target.checked});
+		this.setState({ checked: e.target.checked });
 	}
 
 	render() {
 		return (
 			<tr>
-				<td style={{ border: "1px solid black" }}>
-					<input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}/>
+				<td style={style.tableContent}>
+					<input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)} />
 				</td>
-				<td style={{ border: "1px solid black" }}>{this.props.title}</td>
-				<td style={{ border: "1px solid black" }}>{this.props.children}</td>
+				<td style={style.tableContent}>{this.props.title}</td>
+				<td style={style.tableContent}>{this.props.children}</td>
 			</tr>
 		);
 	}
@@ -62,3 +62,10 @@ class TodoForm extends React.Component {
 		);
 	}
 }
+
+let style = {
+	tableContent: {
+		border: "1px solid black"
+	}
+};
+
